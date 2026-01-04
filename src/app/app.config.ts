7c0f135adda5e85dd43e243fe7000699357349b3
Luca Filip <filip.luca.work@gmail.com>
2026-01-05 01:24:46 +0200

    add primeng

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideStore } from '@ngxs/store';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 import { AppState } from './store';
@@ -9,6 +10,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideStore([AppState])
+    provideStore([AppState]),
+    provideAnimations()
   ]
 };
